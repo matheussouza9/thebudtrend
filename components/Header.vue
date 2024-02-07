db5fc72385f0d876b965ded1e56507131ad101fe
Matheus Souza <matheussouzadecarvalho@gmail.com>
2024-02-07 16:11:25 +0000

    add a header to the default layout

---
+++ components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header class="w-full h-16 bg-transparent">
+    <div
+      class="container h-full mx-auto px-4 flex items-center justify-between"
+    >
+      <NuxtLink to="/">
+        <div class="flex items-center">
+          <img src="/favicon.png" alt="logo" class="h-8 w-8 rounded-lg" />
+          <div class="font-neulis font-light ml-2">The Bud Trend</div>
+        </div>
+      </NuxtLink>
+    </div>
+  </header>
+</template>
