35347c6dd6e9801119b0a20d4b0f4ea860d2cda3
Matheus Souza <matheussouzadecarvalho@gmail.com>
2024-02-07 16:09:21 +0000

    config files stuff
    add custom fonts; set storyblok as the default image provider; set favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import fs from "fs";
+import fs from 'fs'
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    ["@storyblok/nuxt", { accessToken: process.env.STORYBLOK_TOKEN }],
-    "@nuxtjs/tailwindcss",
+    ['@storyblok/nuxt', { accessToken: process.env.STORYBLOK_TOKEN }],
+    '@nuxtjs/tailwindcss',
+    '@nuxt/image',
   ],
-  devServer: {
-    https: {
-      key: fs.readFileSync("./localhost-key.pem").toString(),
-      cert: fs.readFileSync("./localhost.pem").toString(),
+  image: {
+    provider: 'storyblok',
+    storyblok: {
+      baseURL: 'https://a.storyblok.com',
     },
   },
-});
+  devServer: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem').toString(),
+      cert: fs.readFileSync('./localhost.pem').toString(),
+    },
+  },
+  app: {
+    head: {
+      title: 'The Bud Trend',
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }, {}],
+    },
+  },
+  tailwindcss: {
+    configPath: './tailwind.config.ts',
+    cssPath: './global.css',
+  },
+})
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,20 @@
+import type { Config } from 'tailwindcss'
+
 module.exports = {
-  content: ["./storyblok/**/*.{html,js,ts,vue}"],
-};
+  content: [
+    './storyblok/**/*.{html,js,ts,vue}',
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './app.vue',
+    './error.vue',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        neulis: ['NeulisAlt', 'sans-serif'],
+      },
+    },
+  },
+} satisfies Config
