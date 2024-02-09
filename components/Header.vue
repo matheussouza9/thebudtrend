a7c7a82ac7f01874a51cb49428cd8e8c7af41e82
Matheus Souza <matheussouzadecarvalho@gmail.com>
2024-02-09 14:16:42 +0000

    remove application name from the header component

--- components/Header.vue
+++ components/Header.vue
@@ -1,12 +1,11 @@
 <template>
-  <header class="w-full h-16 bg-transparent">
+  <header class="w-full bg-transparent">
     <div
-      class="container h-full mx-auto px-4 flex items-center justify-between"
+      class="container h-full mx-auto px-4 py-2 flex items-center justify-between"
     >
       <NuxtLink to="/">
         <div class="flex items-center">
           <img src="/favicon.png" alt="logo" class="h-8 w-8 rounded-lg" />
-          <div class="font-neulis font-light ml-2">The Bud Trend</div>
         </div>
       </NuxtLink>
     </div>
